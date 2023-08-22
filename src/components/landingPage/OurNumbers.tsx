9bd1cb25af536e601d57ae84b76a9693f47112b4
amgenious <henrytweneboah956@gmail.com>
2023-08-22 12:09:00 +0000

    changed the fonts for the mobile view

--- src/components/landingPage/OurNumbers.tsx
+++ src/components/landingPage/OurNumbers.tsx
@@ -2,18 +2,18 @@ import 'animate.css';
 
 export const OurNumbers = () => {
   return (
-      <div className="flex items-center justify-evenly h-[400px] border" id="container">
+      <div className="flex items-center justify-evenly flex-col py-4 md:flex-row md:py-0 h-fit md:h-[400px] border" id="container">
           <div className="text-center pb-2">
-            <p className="my-2 text-3xl font-bold">Customer Served</p>
-            <p className="text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">21K</p>
+            <p className="my-2 text-xl md:text-3xl font-bold">Customer Served</p>
+            <p className="text-xl md:text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">21K</p>
           </div>
           <div className="text-center pb-2">
-            <p className="my-2 text-3xl font-bold">Transactions Processed</p>
-            <p className="text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">100K</p>
+            <p className="my-2 text-xl md:text-3xl font-bold">Transactions Processed</p>
+            <p className="text-xl md:text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">100K</p>
           </div>
           <div className="text-center ">
-            <p className="my-2 text-3xl font-bold">Positive Review</p>
-            <p className="text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">100</p>
+            <p className="my-2 text-xl md:text-3xl font-bold">Positive Review</p>
+            <p className="text-xl md:text-3xl font-bold animate__animated animate__flipInX animate__delay-1s">100</p>
           </div>
         </div>
   );
