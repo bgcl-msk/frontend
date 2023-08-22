fca2b704ca62455d128890c830f5de0e7bcb1c51
amgenious <henrytweneboah956@gmail.com>
2023-08-22 12:00:20 +0000

    changes the height for the mobile view

--- src/components/landingPage/Carousel.tsx
+++ src/components/landingPage/Carousel.tsx
@@ -24,16 +24,11 @@ export const Carousel = () => {
   return (
     <>
     <div className='pt-14 bg-navbar_bg'>
-    <div className='h-[580px] w-full flex m-auto py-16 px-4 relative group'>
+    <div className='h-[350px] md:h-[580px] w-full flex m-auto py-16 px-4 relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full bg-center bg-contain bg-no-repeat duration-500 flex-1'
       ></div>
-      {/* <div className='flex-1 text-left m-auto '>
-        <h1 className='text-3xl font-bold mb-4'>Customer-Centric Retailing</h1>
-        <p>Our platform is designed to help retailers create personalized shopping experiences, foster lasting connections, and build brand loyalty.</p>
-      </div> */}
-      {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
