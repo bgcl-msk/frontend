aeddeb4e7a33aaa19fbf8728b76c56422937576c
amgenious <henrytweneboah956@gmail.com>
2023-08-22 12:14:49 +0000

    add md: to the width in the classname

--- src/components/landingPage/MskSimplified.tsx
+++ src/components/landingPage/MskSimplified.tsx
@@ -9,7 +9,7 @@ export const MskSimplified = () => {
             <img src={laptop} className='w-[800px]'></img>
         </div>
         <div className='flex-1 m-auto msk_simp_words'>
-          <div className="w-[70%]">
+          <div className="md:w-[70%]">
         <p className='text-3xl my-6'><b>MSK simpilifies the services of your web applications.</b></p>
         <p>Discover a new level of streamlined efficiency with our cutting-edge application. Seamlessly manage every aspect of your web services with advanced user management, shop and warehouse oversight, budget tracking, sales optimization, and more.</p>
         </div>
